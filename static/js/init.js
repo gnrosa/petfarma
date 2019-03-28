bb7c65e81c50f6e5128c8eb660f5cdcaabdca2ac
Guilherme Nunes Rosa <guilherme.rosa@ciccr-sp.interno>
2019-03-28 17:45:58 -0300

    change layout

---
+++ static/js/init.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+  $('.carousel').carousel(
+  {
+    dist: 0,
+    padding: 0,
+    fullWidth: true,
+    indicators: true,
+    duration: 100,
+  }
+  );
+});
+
+$(document).ready(function(){
+  $('.tabs').tabs();
+});
